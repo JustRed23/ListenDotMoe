62594df956f4feb8a273426dc0ed4492faf8362b
Manuel <manuel.bormans@gmail.com>
2021-03-22 20:29:53 +0100

    Put processing to a different thread and updated version

--- src/main/java/dev/JustRed23/ListenDotMoe/ListenDotMoe.java
+++ src/main/java/dev/JustRed23/ListenDotMoe/ListenDotMoe.java
@@ -5,50 +5,53 @@ import com.google.gson.JsonParser;
 import dev.JustRed23.ListenDotMoe.Endpoint.LDMEndpoint;
 import dev.JustRed23.ListenDotMoe.Music.Song;
 import dev.JustRed23.ListenDotMoe.Music.SongUpdateEvent;
-import dev.JustRed23.ListenDotMoe.Utils.Logger;
 import jakarta.websocket.CloseReason;
 import org.fusesource.jansi.AnsiConsole;
 
 import static dev.JustRed23.ListenDotMoe.Utils.Logger.*;
 
-public class ListenDotMoe implements Runnable {
+public class ListenDotMoe {
 
-    public static final double VERSION = 1.0;
+    public static final double VERSION = 1.1;
 
     private static LDMEndpoint endpoint;
-    private SongUpdateEvent songUpdateEvent;
+    private static SongUpdateEvent songUpdateEvent;
 
     public static final String LDM_ALBUM_ENDPOINT = "https://cdn.listen.moe/covers/";
     public static final String LDM_ARTISTS_ENDPOINT = "https://cdn.listen.moe/artists/";
 
+    private boolean running = false;
+
     public void start() {
+        if (running) return;
+        running = true;
         AnsiConsole.systemInstall();
 
-        Thread thread = new Thread(this, "ListenDotMoe");
+        Thread thread = new Thread(() -> {
+            info("Starting ListenDotMoe V" + VERSION);
+            endpoint = new LDMEndpoint("wss://listen.moe/gateway_v2");
+            endpoint.addMessageHandler(ListenDotMoe::processMessage);
+
+            try {
+                endpoint.closeLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            info("Closed client");
+        }, "ListenDotMoe");
         thread.start();
     }
 
     public void stop() {
+        if (!running) return;
+        running = false;
         info("Closing client");
         endpoint.close(new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE, "Closing client"));
         AnsiConsole.systemUninstall();
     }
 
-    public void run() {
-        info("Starting ListenDotMoe V" + VERSION);
-        endpoint = new LDMEndpoint("wss://listen.moe/gateway_v2");
-        endpoint.addMessageHandler(this::processMessage);
-
-        try {
-            endpoint.closeLatch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        info("Closed client");
-    }
-
-    private synchronized void processMessage(String message) {
+    private static synchronized void processMessage(String message) {
         JsonObject json = (JsonObject) JsonParser.parseString(message);
 
         switch (json.get("op").getAsInt()) {
@@ -61,7 +64,6 @@ public class ListenDotMoe implements Runnable {
                 Song song = new Song(json);
                 if (songUpdateEvent != null)
                     songUpdateEvent.onSongUpdate(song);
-                Logger.debug(songUpdateEvent);
                 break;
             case 10:
                 debug("Received heartbeat");
